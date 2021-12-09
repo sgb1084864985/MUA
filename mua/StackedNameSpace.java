de00ba8f309c83324a4703289f642102dc5c358d
zlz <3190101094@zju.edu.cn>
2021-12-09 19:54:03 +0800

    change stackednamespace to linkedlist

--- mua/StackedNameSpace.java
+++ mua/StackedNameSpace.java
@@ -2,9 +2,13 @@ package mua;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class StackedNameSpace implements NameSpace{
-    ArrayList<HashMap<String,value>> namespaces=new ArrayList<>();
+    LinkedList<HashMap<String,value>> namespaces=new LinkedList<>();
     static final int LIMITED_LEVEL=3;
     public void addEmptySpace(){
         namespaces.add(new HashMap<>());
@@ -16,11 +20,11 @@ public class StackedNameSpace implements NameSpace{
         namespaces.addAll(space.namespaces);
     }
     public HashMap<String,value> getFirstSpace(){
-        return namespaces.get(0);
+        return namespaces.getFirst();
     }
 
     public HashMap<String,value> getLastSpace(){
-        return namespaces.get(namespaces.size()-1);
+        return namespaces.getLast();
     }
 
     public StackedNameSpace cloneWithNewSpace(){
@@ -32,12 +36,24 @@ public class StackedNameSpace implements NameSpace{
 
     public StackedNameSpace limitedCloneWithNewSpace(){
         StackedNameSpace other = new StackedNameSpace();
-        other.namespaces.add(namespaces.get(0));
-        for(int i=namespaces.size()-LIMITED_LEVEL+2;i<namespaces.size();i++){
-            if(i>0){
-                other.namespaces.add(namespaces.get(i));
-            }            
+        other.namespaces.add(namespaces.getFirst());
+        try{
+            ListIterator<HashMap<String, value>> listIt=namespaces.listIterator
+            (Math.
+                max(1,namespaces.size()-LIMITED_LEVEL+2)
+            );
+
+            while(listIt.hasNext()){
+                other.namespaces.add(listIt.next());
+            }
         }
+        catch(IndexOutOfBoundsException ex){}
+
+        // for(int i=namespaces.size()-LIMITED_LEVEL+2;i<namespaces.size();i++){
+        //     if(i>0){
+        //         other.namespaces.add(namespaces.get(i));
+        //     }            
+        // }
         other.namespaces.add(new HashMap<>());
         return other;
     }
@@ -45,16 +61,25 @@ public class StackedNameSpace implements NameSpace{
     @Override
     public value get(String name) {
         value v = null;
-        for(int i= namespaces.size()-1;i>=0;i--){
-            v=namespaces.get(i).get(name);
-            if(v!=null) return v;
+        // for(int i= namespaces.size()-1;i>=0;i--){
+        //     v=namespaces.get(i).get(name);
+        //     if(v!=null) return v;
+        // }
+
+        Iterator<HashMap<String,value>> it = namespaces.descendingIterator();
+        while(it.hasNext()){
+            v=it.next().get(name);
+            if(v!=null){
+                return v;
+            }
         }
-        return v;
+
+        return null;
     }
 
     @Override
     public void set(String name, value v) {
-        namespaces.get(namespaces.size()-1).put(name,v);
+        getLastSpace().put(name,v);
     }
 
     public void export(String name,value v){
